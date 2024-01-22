6313f8be17a4f0148c6e275d720166873d907318
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2024-01-22 07:53:24 -0300

    get search products

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,7 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-function SearchPage() {
+import { api } from "@/data/api";
+import { Product } from "@/data/types/products";
+
+interface SearchPageProps {
+  searchParams: {
+    q: string;
+  };
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
+
+  const products = await response.json();
+
+  return products;
+}
+
+async function SearchPage({ searchParams }: SearchPageProps) {
+  const { q: query } = searchParams;
+
+  if (!query) {
+    redirect("/");
+  }
+
+  const products = await searchProducts(query);
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
@@ -9,30 +39,32 @@ function SearchPage() {
       </p>
 
       <div className="grid grid-cols-3 gap-6">
-        <Link
-          href={`/product/${"asflkj"}`}
-          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        >
-          <Image
-            src={"/moletom-ai-side.png"}
-            width={400}
-            height={400}
-            quality={100}
-            alt="product"
-            className="group-hover:scale-105 transition-transform duration-500"
-          />
+        {products.map((product) => (
+          <Link
+            href={`/product/${"asflkj"}`}
+            className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+            key={product.id.toString()}
+          >
+            <Image
+              src={product.image}
+              width={400}
+              height={400}
+              quality={100}
+              alt="product"
+              className="group-hover:scale-105 transition-transform duration-500"
+            />
 
-          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">{"title"}</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {/* {product.price.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })} */}
-              99
-            </span>
-          </div>
-        </Link>
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
